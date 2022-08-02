a84b0158148e8c58cd59ae5391dababe082c6fcb
HenryWu1113 <bug910284@gmail.com>
2022-08-02 16:34:15 +0800

    新增products的middleware和路由

--- routes/products.js
+++ routes/products.js
@@ -1,14 +1,22 @@
 import express from 'express'
 import content from '../middleware/content.js'
 import * as auth from '../middleware/auth.js'
-import upload from '../middleware/upload.js'
 import admin from '../middleware/admin.js'
+import upload from '../middleware/upload.js'
 import {
-  creatProduct
+  createProduct,
+  getProducts,
+  getAllProducts,
+  getProduct,
+  editProduct
 } from '../controllers/products.js'
 
 const router = express.Router()
 
-router.post('/', content('multipart/form-data'), auth.jwt, admin, upload, creatProduct)
+router.post('/', content('multipart/form-data'), auth.jwt, admin, upload, createProduct)
+router.get('/', getProducts)
+router.get('/all', auth.jwt, admin, getAllProducts)
+router.get('/:id', getProduct)
+router.patch('/', content('multipart/form-data'), auth.jwt, admin, upload, editProduct)
 
 export default router
