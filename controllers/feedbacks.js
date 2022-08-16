9790f6629fc3e4325807ddcf5678699320e5499b
HenryWu1113 <bug910284@gmail.com>
2022-08-16 23:40:27 +0800

    新增 feedback 的時間排序

--- controllers/feedbacks.js
+++ controllers/feedbacks.js
@@ -5,7 +5,7 @@ export const createFeedback = async (req, res) => {
   try {
     let result = users.findById(req.user._id)
     result = await (await feedbacks.create({ user: req.user._id, comment: req.body.comment })).populate('user')
-    res.status(200).send({ success: true, message: '', result: result })
+    res.status(200).send({ success: true, message: '', result })
   } catch (error) {
     console.log(error)
     res.status(500).send({ success: false, message: '伺服器錯誤' })
@@ -14,8 +14,8 @@ export const createFeedback = async (req, res) => {
 
 export const getAllFeedback = async (req, res) => {
   try {
-    const result = await feedbacks.find().populate('user')
-    res.status(200).send({ success: true, message: '', result: result })
+    const result = await feedbacks.find().populate('user').sort({ date: -1 })
+    res.status(200).send({ success: true, message: '', result })
   } catch (error) {
     res.status(500).send({ success: false, message: '伺服器錯誤' })
   }
